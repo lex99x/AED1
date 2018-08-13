e7263c64092fa4a3e50f656734d6d4d1200a3973
André Rocha <andrerochaspan@gmail.com>
2018-08-13 10:41:27 -0400

    Lab 1

---
+++ 2018.2/labs/lab1_3.c
@@ -0,0 +1,49 @@
+// Enunciado
+// Distância entre Objetos
+
+// A distância entre as duas árvores pode ser calculada pelo observador usando a lei dos cossenos:
+
+// c = sqrt(a * a + b * b - 2 * a * b * cos(y));
+
+// Faça um programa que tenha como entrada, nessa ordem:
+// 1. a distância a entre o observador e a primeira árvore.
+// 2. a distância b entre o observador e a segunda árvore.
+// 3. o angulo y entre a e b (em graus).
+
+// Como saída, seu programa deverá imprimir, com duas casas decimais, o valor de c,
+// correspondente à distância entre as duas árvores.
+
+// Dicas
+// Para tirar a raiz quadrada, você pode usar a função sqrt da biblioteca math
+// A função cos do também da biblioteca math para calcular o coseno de y,
+// lembrando que ela recebe como argumento um valor em radianos.
+// Para converter um ângulo y de graus para radianos, use a expressão: (y ∗ π)/180.0
+
+// Exemplo de Entrada
+// 12.5
+// 17.2
+// 64.9
+// Exemplo de Saída
+// 16.42
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159265358979323846264338327950288
+
+int main(void){
+
+	double a = 0.0, b = 0.0, c = 0.0, y = 0.0;
+
+	scanf("%lf", &a);
+	scanf("%lf", &b);
+	scanf("%lf", &y);
+
+	y = (y * PI)/180.0;
+
+	c = sqrt(a * a + b * b - 2 * a * b * cos(y));
+
+	printf("%.2lf\n", c);
+
+	return 0;
+
+}
---
+++ 2018.2/labs/lab1_4.c
@@ -0,0 +1,46 @@
+// Enunciado
+// Fórmula da Panqueca
+// Para evitar desperdício na hora de fazer panquecas, Jebediah Kerman derivou
+// a seguinte fórmula para calcular a quantidade de massa (em ml) necessária:
+
+// M = (D * D * T * PI * P)/4;
+
+// onde:
+
+// D (cm) é o diâmetro da frigideira.
+// T (cm) é a espessura da panqueca (antes de ser enrolada com o recheio).
+// P é a quantidade de panquecas que se quer fazer.
+// Faça um programa que tenha como entrada estes valores nesta mesma ordem.
+// Como saída, seu programa deve calcular e imprimir, com duas casas decimais,
+// a quantidade de massa (M) necessária para fazer as panquecas.
+
+// Dicas
+// Use o valor da constante pi fornecida pelo módulo math.
+// Um valor aproximado pode levar a uma resposta diferente
+// daquela esperada pelo CodeBench.
+// Exemplo de Entrada
+// 23.0
+// 0.08
+// 14
+// Exemplo de Saída
+// 465.33
+
+#include <stdio.h>
+#define PI 3.14159265358979323846264338327950288
+
+int main(void){
+
+	double D = 0.0, T = 0.0, M = 0.0;
+	int P = 0;
+
+	scanf("%lf", &D);
+	scanf("%lf", &T);
+	scanf("%d", &P);
+
+	M = (D * D * T * PI * P)/4;
+
+	printf("%.2lf\n", M);
+
+	return 0;
+
+}
