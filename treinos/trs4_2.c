ffb193fc09d9c942d867494e861ce23a542296ad
André Rocha <andrerochaspan@gmail.com>
2018-09-02 16:00:53 -0400

    questão 2 do treinamento 4 corrigida

--- treinos/trs4_2.c
+++ treinos/trs4_2.c
@@ -1,12 +1,17 @@
 // Enunciado
-// O projeto do motor vem sendo ajustado para que se torne mais eficiente
-// na retomada de rotação.  Em um serie S foram registradosas rotações
-// alcanças durante os testes, na escala de 1(um) segundo. A série S foi
-// apresentada para você com um pedido: projeto um programa que encontre
-// os eventos de retormada de rotação, que tenham como característica valor
-// maior ou igual ao registrado antes da redução da rotação. A série S termina
-// quando um valor Negativo para as rotações for lido e quando não houver
-// registro de evento, a seguinte mensagem deve ser gerada: sem eventos
+// Um projeto de motor vem sendo ajustado para que se torne mais eficiente.
+// A medida usada para aferir a eficiência é a retomada de rotação, em testes
+// de aceleração e desaceleração do motor, que são representados por PICOS
+// definidos por valores consecultivos de uma tripla (ri-1, ri, ri+1) onde ri
+// é maior que os outros dois valores ri-1 e ri+1 (ver figura abaixo). A altura
+// de cada pico é dada pela diferença entre as medições ri-1 e ri. Por exemplo,
+// o pico definido pelas rotações (8, 15, 10) tem altura 15-8=7.
+// Em um serie S foram registradas as rotações observadas durante os testes.
+// Projete e implemente um programa que encontre os eventos de retormada de
+// rotação que ateste a eficiência do motor. Eventos de retomada são aqueles
+// representados no gráfico acima por picos consecultivos, Pk e Pk+1.
+// Nesse caso será considerado uma retomada eficiente se o pico Pk+1 tiver
+// altura maior que o pico Pk.
 // Exemplo de Entrada
 // 10 15 10 16 6  7 8 8 10 3 -1
 // Exemplo de Saída
@@ -16,31 +21,30 @@
 
 int main(void){
 
-	int rot1, rot2, rot3, alt;
-	int mrot1, mrot2, mrot3, malt;
-	short primVez =  1, mpico = 0;
+	int rot1, rot2, rot3, alt, altAnt, contPicoAnt = 0, cont = 3;
+	short primVez = 1, eventos = 0;
 
 	scanf("%d %d %d", &rot1, &rot2, &rot3);
 
-	alt = malt = rot2 - rot1;
-
 	while(rot3 >= 0){
 
-		if(rot2 > rot1 && rot2 > rot3){
+		if(rot1 < rot2 && rot2 > rot3){
 
 			alt = rot2 - rot1;
 
-			if(alt >= malt){
+			if(primVez){
 
-				malt = alt;
-				mrot1 = rot1;
-				mrot2 = rot2;
-				mrot3 = rot3;
+				primVez = 0;
 
-				if(primVez) primVez = 0;
-				else mpico = 1;
+			}else if((alt >= altAnt) && ((cont - contPicoAnt) == 2)){
+
+				eventos = 1;
+				printf("%d %d %d\n", rot1, rot2, rot3);
 
 			}
+
+			altAnt = alt;
+			contPicoAnt = cont;
 
 		}
 
@@ -49,10 +53,11 @@ int main(void){
 
 		scanf("%d", &rot3);
 
+		cont++;
+
 	}
 
-	if(mpico) printf("%d %d %d\n", mrot1, mrot2, mrot3);
-	else printf("sem eventos\n");
+	if(!eventos) printf("sem eventos\n");
 
 	return 0;
 
